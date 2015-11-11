92bab2af9b547c244cbfd50e92e60252f2d68493
Alfonso Guadalupe Baeza Rosales <abrosales@hotmail.com>
2015-11-11 17:14:54 -0600

    avances hasta suma
    en ventas y encender gps sin mosrar la ubicacion

--- RegistroMovistar/app/src/main/java/com/example/alfonsogbaezar/registromovistar/MainActivity.java
+++ RegistroMovistar/app/src/main/java/com/example/alfonsogbaezar/registromovistar/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.alfonsogbaezar.registromovistar;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private LocationManager locMana;
+    private LocationListener locListen;
 
+    AlertDialog alert = null;
+
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +40,15 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+
+
+
+
     }
 
-    @Override
+
+
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -45,9 +71,34 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     public void onclick(View view){
-        Intent i=new Intent(this,Principal.class);
-        startActivity(i);
-        finish();
+        locMana = (LocationManager) getSystemService( LOCATION_SERVICE );
+        if ( !locMana.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            AlertNoGps();
+        }else {
+            Intent i = new Intent(this, Principal.class);
+            startActivity(i);
+            finish();
+            locMana=null;
+        }
 
+    }
+    private void AlertNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("El sistema GPS esta desactivado, ¿Desea activarlo?")
+                .setCancelable(false)
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                        alert = builder.create();
+                        alert.show();
+                    }
+                });
+        alert = builder.create();
+        alert.show();
     }
 }
--- RegistroMovistar/app/src/main/java/com/example/alfonsogbaezar/registromovistar/Principal.java
+++ RegistroMovistar/app/src/main/java/com/example/alfonsogbaezar/registromovistar/Principal.java
@@ -1,9 +1,15 @@
 package com.example.alfonsogbaezar.registromovistar;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,13 +19,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    View asit;
+    private View asit;
     View notifi;
     View vent;
     View estad;
+    private LocationManager locManager;
+    private LocationListener locListener;
+    EditText loca;
+    AlertDialog alert = null;
+    TextView maschips;
+    TextView maspalnes;
+    TextView masportas;
+    TextView masequipos;
+    TextView menoschips;
+    TextView menosplanes;
+    TextView menosportas;
+    TextView menosequipos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,8 +59,62 @@ public class Principal extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         super.setTitle("Asistencia");
         asit.setVisibility(View.VISIBLE);
+        comenzarLocalizacion();
+
+    }
+    private void comenzarLocalizacion()
+    {
+        //Obtenemos una referencia al LocationManager
+        locManager =(LocationManager)getSystemService(LOCATION_SERVICE);
+
+        //Obtenemos la última posición conocida
+        Location location =locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Toast.makeText(this, (CharSequence) location, Toast.LENGTH_SHORT).show();
+        //Mostramos la última posición conocida
+        mostrarPosicion(location);
+
+        //Nos registramos para recibir actualizaciones de la posición
+        locListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                mostrarPosicion(location);
+            }
+            public void onProviderDisabled(String provider){
+
+
+            }
+            public void onProviderEnabled(String provider){
+
+            }
+            public void onStatusChanged(String provider, int status, Bundle extras){
+
+            }
+        };
+
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0, locListener);
     }
 
+
+
+    private void mostrarPosicion(Location loc) {
+        loca= (EditText) findViewById(R.id.txt_localizacion);
+        if(loc != null)
+        {
+            Double x=loc.getLatitude();
+            Double y=loc.getLongitude();
+            String posi= Double.toString(x)+","+Double.toString(y);
+
+            Toast.makeText(this, posi, Toast.LENGTH_SHORT).show();
+            loca.setText(posi);
+
+
+            Log.i("", String.valueOf(loc.getLatitude() + " - " + String.valueOf(loc.getLongitude())));
+        }
+        else
+        {
+            Toast.makeText(this, "vacio", Toast.LENGTH_SHORT).show();
+
+        }
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -117,5 +192,61 @@ public class Principal extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public void onclickcontrolesventa(View view){
+        maschips= (TextView) findViewById(R.id.txt_numchips);
+        maspalnes=(TextView) findViewById(R.id.txt_numplanes);
+        masportas=(TextView)  findViewById(R.id.txt_numporta);
+        masequipos=(TextView) findViewById(R.id.txt_numequiposnuevos);
+
+        int id = view.getId();
+        int chip=Integer.parseInt(maschips.getText().toString());
+        int plan=Integer.parseInt(maspalnes.getText().toString());
+        int porta=Integer.parseInt(masportas.getText().toString());
+        int eqnu=Integer.parseInt(masequipos.getText().toString());
+        if (id== R.id.btnmas_chips){
+            chip=chip+1;
+            maschips.setText(Integer.toString(chip));
+
+        }else if(id== R.id.btnmas_planes){
+            plan=plan+1;
+            maspalnes.setText(Integer.toString(plan));
+
+        } else if(id== R.id.btnmas_portas){
+            porta=porta+1;
+            masportas.setText(Integer.toString(porta));
+
+        }else if(id== R.id.btnmas_equipos){
+
+            eqnu=eqnu+1;
+            masequipos.setText(Integer.toString(eqnu));
+        }else if(id== R.id.btnmenos_chips){
+            if(chip>0){
+                chip=chip-1;
+                maschips.setText(Integer.toString(chip));
+            }
+
+        }else if(id== R.id.btnmenos_planes){
+            if(plan>0){
+                plan=plan-1;
+                maspalnes.setText(Integer.toString(plan));
+            }
+
+        }else if(id== R.id.btnmenos_portas){
+            if(porta>0){
+                porta=chip-1;
+                masportas.setText(Integer.toString(porta));
+            }
+
+        }else if(id== R.id.btnmenos_equipos){
+            if(eqnu>0){
+                eqnu=eqnu-1;
+                masequipos.setText(Integer.toString(eqnu));
+            }
+
+        }
+
+
+
     }
 }
